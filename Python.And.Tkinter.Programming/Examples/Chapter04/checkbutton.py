bc5ab7b4e449df36c8d404f3200ce159d403d3c0
hexu <hexu_1985@sina.com>
2024-03-07 15:25:58 +0800

    update Python.And.Tkinter.Programming/Examples/Chapter04

--- Python.And.Tkinter.Programming/Examples/Chapter04/checkbutton.py
+++ Python.And.Tkinter.Programming/Examples/Chapter04/checkbutton.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 
 class Dummy: pass
 var = Dummy()
@@ -12,6 +12,6 @@ for castmember, row, col, status in [
     ('Michael Palin',2,0,NORMAL), ('Terry Gilliam', 2,1,NORMAL)]:
     setattr(var, castmember, IntVar())
     Checkbutton(root, text=castmember, state=status, anchor=W,
-      variable = getattr(var, castmember)).grid(row=row, col=col, sticky=W)
+      variable = getattr(var, castmember)).grid(row=row, column=col, sticky=W)
 root.mainloop()
 
