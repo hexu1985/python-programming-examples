8c0136f35dbb230ded271c32f9102c400b46d709
hexu <hexu_1985@sina.com>
2024-03-18 10:27:26 +0800

    update Python.GUI.Tkinter.Rookie.Programming/chapter-19/ch19_14.py

--- Python.GUI.Tkinter.Rookie.Programming/chapter-19/ch19_14.py
+++ Python.GUI.Tkinter.Rookie.Programming/chapter-19/ch19_14.py
@@ -3,12 +3,12 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 tk = Tk()
-img = Image.open("kobe.jpg")
+img = Image.open("cover.jpg")
 myPic = ImageTk.PhotoImage(img)
 
 canvas = Canvas(tk, width=img.size[0]+40,
                 height=img.size[1]+30, bg='yellow')
-canvas.create_image(200,15,anchor=NW,image=myPic)
+canvas.create_image(20,15,anchor=NW,image=myPic)
 canvas.pack(fill=BOTH,expand=True)
 
 tk.mainloop()
