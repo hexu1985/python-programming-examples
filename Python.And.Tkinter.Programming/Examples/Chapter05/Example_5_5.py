3c2e0a0d6f27740a4c5872578492b3fffa04d645
hexu <hexu_1985@sina.com>
2025-03-09 20:54:32 +0800

    update Python.And.Tkinter.Programming/Examples/Chapter05

--- Python.And.Tkinter.Programming/Examples/Chapter05/Example_5_5.py
+++ Python.And.Tkinter.Programming/Examples/Chapter05/Example_5_5.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 
 class App:
     def __init__(self, master):
@@ -9,7 +9,7 @@ class App:
         Button(fm, text='Right').pack(side=LEFT)
         fm.pack(fill=BOTH, expand=YES)
 
-        
+
 root = Tk()
 root.option_add('*font', ('verdana', 12, 'bold'))
 root.title("Pack - Example 5")
